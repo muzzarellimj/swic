14e98a175080572803acc8df82cf1b5f07545cfe
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-03-07 08:18:50 -0500

    Include assignment one class, Schedule

---
+++ computer-science-i-java/assignment-1/src/Schedule.java
@@ -0,0 +1,18 @@
+/**
+ * An application to print my course schedule. Completed on 8/26/2020.
+ *
+ * @author Michael Muzzarelli, michael.muzzarelli@student.swic.edu
+ */
+public class Schedule {
+
+    public static void main(String[] args) {
+        System.out.println("COURSE SCHEDULE:");
+        System.out.println("Name:      Michael Muzzarelli");
+        System.out.println("Email:     michael.muzzarelli@student.swic.edu");
+        System.out.println("Semester:  Fall 2020");
+        System.out.println(" ");
+        System.out.println("COURSE         DAY        TIME          ROOM       INSTRUCTOR");
+        System.out.println("MATH 171-001   MoTuThFr   9:00A-9:50A   MC2183     Collins");
+        System.out.println("PSYC 225-IN1   Internet   Internet      Internet   Hunter");
+    }
+}
